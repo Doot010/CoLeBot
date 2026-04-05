1ea40ccff1ca76ae807f7a2e46d37efe0d586990
Doot <120121719+Doot010@users.noreply.github.com>
2026-04-05 16:32:39 -0600

    Update TxRx_ButtonJoystick.ino

--- TxRx_ButtonJoystick.ino
+++ TxRx_ButtonJoystick.ino
@@ -1,39 +1,61 @@
 #include "WifiPort2.h"
+
 #include <Servo.h>
+
 
 //TX VARIABLES
 
 int b1Pin;
+
 int b2Pin;
+
 int b3Pin;
+
 int b4Pin;
+
 int JPin;
+
 int XPin;
+
 int YPin;
+
 
 //RX VARIABLES
 
 //Right Motor
+
 int Rena1; // Arduino pin connected to Enable1 of H-Bridge
+
 int Rdrive1A;// Arduino pin connected to In1 of H-Bridge
+
 int Rdrive2A;// Arduino pin connected to In2 of H-Bridge
 
 
 //Left Motor
+
 int Lena1; // Arduino pin connected to Enable1 of H-Bridge
+
 int Ldrive1A;// Arduino pin connected to In1 of H-Bridge
+
 int Ldrive2A; // Arduino pin connected to In2 of H-Bridge
 
 
 //Stepper Motors
+
 bool initPos = 1;
+
 int clawAngle = 20;
+
 int armAngle = 70;
+
 int clawChange = clawAngle;
+
 int armChange = armAngle;
 
 Servo Claw;
+
 Servo Arm;
+
 
 // A structure, similar to our servo and stepper motors, but this one conatins variables to be transmitted
 
@@ -58,12 +80,15 @@ struct DataPacket {
 
   int joystickButton;                //YOU should wire up a simple resistor (220 ohm) circuit and manually probe it with a wire connected to A0
 
+
 };
 
 
 DataPacket data;//data.AnalogCheck
 
+
 //gloabl vars are outside datapacket
+
 
 WifiPort<DataPacket> WifiSerial;
 
@@ -103,7 +128,7 @@ void setup() {
 
     JPin = 2;
 
-   XPin = A0;
+    XPin = A0;
 
     YPin = A1;
 
@@ -125,31 +150,55 @@ void setup() {
 
 
   if ((WifiSerial.getPortType() == WifiPortType::Receiver || WifiSerial.getPortType() == WifiPortType::Emulator)){
+
     //RECIEVER BOARD
 
+
     //Right Motor
+
     Rena1 = 3; // Arduino pin connected to Enable1 of H-Bridge
+
     Rdrive1A = 2; // Arduino pin connected to In1 of H-Bridge
+
     Rdrive2A = 4; // Arduino pin connected to In2 of H-Bridge
 
+
     //Left Motor
+
     Lena1 = 6; // Arduino pin connected to Enable1 of H-Bridge
+
     Ldrive1A = 5; // Arduino pin connected to In1 of H-Bridge
+
     Ldrive2A = 7; // Arduino pin connected to In2 of H-Bridge
 
+
     Claw.attach(A1);
+
     Arm.attach(A0);
 
     Claw.write(clawAngle);
+
     Arm.write(armAngle);
+
+    delay(100);
+
+
     //Pin Moding it
+
     pinMode(Rena1,OUTPUT);
+
     pinMode(Rdrive1A,OUTPUT);
+
     pinMode(Rdrive2A,OUTPUT);
+
     pinMode(Lena1,OUTPUT); // set digital pin modes to output
+
     pinMode(Ldrive1A,OUTPUT);
+
     pinMode(Ldrive2A,OUTPUT);
+
   }
+
 }
 
 
@@ -166,6 +215,7 @@ void loop() {
 
     WifiSerial.autoReconnect();//try and connect
 
+
     //Tx stuff below
 
     dataCheck();
@@ -173,11 +223,17 @@ void loop() {
     //Tx stuff above
 
     data.button1=digitalRead(3);
+
     data.button2=digitalRead(4);
+
     data.button3=digitalRead(5);
+
     data.button4=digitalRead(6);
+
     data.joystickX=analogRead(A0);
+
     data.joystickY=analogRead(A1);
+
     data.joystickButton=digitalRead(2);
 
 
@@ -187,6 +243,7 @@ void loop() {
 
       Serial.println("Wifi Send Problem");//oh no it didn't send --> it iwll try and re-connect at the start of the loop
 
+
   }
 
 
@@ -195,177 +252,352 @@ void loop() {
 
   if ((WifiSerial.getPortType() == WifiPortType::Receiver || WifiSerial.getPortType() == WifiPortType::Emulator) && WifiSerial.checkForData()) {
 
+
     data = WifiSerial.getData();//received and unpack data structure
+
 
     //all Rx stuff below
 
     dataCheck();
 
+
     //Skid Steering Stuff
 
     const int fwdSpeed = map(data.joystickX, 540,1023,0,125); //Find proportional value of 1028 as percentage of 255 for analogWrite
+
     const int bckSpeed = map(data.joystickX, 480,0,0,125);
 
-    const int rSpeed = map(data.joystickY, 540,1023,0,255); //Same idea but for turning
+
+    const int rSpeed = map(data.joystickY, 540,1023,0,255); //lspeed and rspeed is actually front and back,they're switched
+
     const int lSpeed = map(data.joystickY, 480,0,0,255);
 
 
     if(data.joystickX > 540){   //TURNING LEFT
+
       digitalWrite(Rdrive1A, LOW); 
+
       digitalWrite(Rdrive2A, HIGH);
 
       digitalWrite(Ldrive1A, LOW); 
+
       digitalWrite(Ldrive2A, HIGH);
 
       Serial.println(fwdSpeed);
 
       analogWrite(Rena1, fwdSpeed);
+
       analogWrite(Lena1, fwdSpeed);
 
     }
 
     else if (data.joystickX < 480){ //TURNING RIGHT
+
       digitalWrite(Rdrive1A, HIGH); 
+
       digitalWrite(Rdrive2A, LOW);
 
       digitalWrite(Ldrive1A, HIGH); 
+
       digitalWrite(Ldrive2A, LOW);
 
       analogWrite(Rena1, bckSpeed);
+
       analogWrite(Lena1, bckSpeed);
 
     }
 
     else if (data.joystickY > 540){ //FORWARDS
+
       digitalWrite(Rdrive1A, HIGH); 
+
       digitalWrite(Rdrive2A, LOW);
 
       digitalWrite(Ldrive1A, LOW); 
+
       digitalWrite(Ldrive2A, HIGH);
 
       analogWrite(Rena1, rSpeed);
+
       analogWrite(Lena1, rSpeed);
 
     }
 
     else if (data.joystickY < 480){ //BACKWARDS
+
       digitalWrite(Rdrive1A, LOW); 
+
       digitalWrite(Rdrive2A, HIGH);
 
       digitalWrite(Ldrive1A, HIGH); 
+
       digitalWrite(Ldrive2A, LOW);
 
       analogWrite(Rena1, lSpeed);
+
       analogWrite(Lena1, lSpeed);
 
     }
 
     else { //No movement when joystick centered
+
       digitalWrite(Rdrive1A, LOW); 
+
       digitalWrite(Rdrive2A, LOW);
 
       digitalWrite(Ldrive1A, LOW); 
+
       digitalWrite(Ldrive2A, LOW);
 
       analogWrite(Rena1, 0);
+
       analogWrite(Lena1, 0);
+
     }
+
+
+ 
 
     //Stepper Control
 
-    if(data.button1 == LOW){ // open claw
+    if(data.button3 == LOW){ // open claw //5
+
       clawAngle = clawAngle -5;
+
     }
 
-    if(data.button2 == LOW){ // close claw
+    if(data.button2 == LOW){ // close claw //4
+
       clawAngle = clawAngle + 5;
+
     }
 
 
-    if(data.button3 == LOW){ // lower arm
+    if(data.button4 == LOW){ // lower arm//3 
+
       armAngle = armAngle - 5;
+
     }
 
-    if(data.button4 == LOW){ // raise arm
+    if(data.button1 == LOW){ // raise arm //6
+
       armAngle = armAngle + 5;
+
       Serial.println(armAngle);
+
     }
+
+    if(data.joystickButton == LOW){ //use the joystick button to trigger dance since not being used
+
+      dance();
+
+    }
+
+ 
+
+
     clawAngle = constrain(clawAngle, 10, 55); //beautiful constraint functions
+
     armAngle = constrain(armAngle, 50, 125);
 
     if(clawAngle != clawChange){ //claw movement and limits
+
       Claw.write(clawAngle);
+
       clawChange = clawAngle;
 
       delay(15);
+
     }
 
     if(armAngle != armChange){ //arm movement and limits
+
       Arm.write(armAngle);
+
       armChange = armAngle;
 
       delay(15);
+
     }
+
+
       //gripper range: 0-45, 10 is safe start - must make manually
+
       //arm range: 20-110, 70 is safe start - must make manually
+
     //all RX stuff above
+
+
   }
+
+
+  delay(10); // update delay after you get it working to be a smaller number like 10ms to account for WiFi transmission overhead
+
 }
+
 
 void dataCheck(){
+
   Serial.print("Time Check: " );
+
   Serial.print(data.AnalogCheck);
+
   Serial.print( "  Button 1: " );
+
   Serial.print(data.button1);
+
   Serial.print( "  Button 2: " );
+
   Serial.print(data.button2);
+
   Serial.print( "  Button 3: " );
+
   Serial.print(data.button3);
+
   Serial.print( "  Button 4: " );
+
   Serial.println(data.button4);
+
   Serial.print( "  Joystick X: " );
+
   Serial.print(data.joystickX);
+
   Serial.print( "  Joystick Y: " );
+
   Serial.print(data.joystickY);
+
   Serial.print( "  Joystick Button: " );
+
   Serial.println(data.joystickButton);
+
   Serial.println("");
+
 }
 
-void dance(){
-  digitalWrite(Rdrive1A, LOW); 
+ 
+
+void dance(){ //make sure to fix with delays, wasn't working before
+
+ 
+
+  digitalWrite(Rdrive1A, LOW); //spin left
+
   digitalWrite(Rdrive2A, HIGH);
 
-  digitalWrite(Ldrive1A, LOW); 
+  digitalWrite(Ldrive1A, LOW);
+
   digitalWrite(Ldrive2A, HIGH);
 
-  analogWrite(Rena1, 255);
-  analogWrite(Lena1, 255);
-  for(int i = 50; i <= 125;i++){
-    Arm.write(i);
-  }
-  for(int i = 125; i >= 50;i--){
-    Arm.write(i);
+ 
+
+  analogWrite(Rena1, 200); //just going to use numbers for speed
+
+  analogWrite(Lena1, 200);
+
+ 
+
+  for(int j = 0; j < 2; j++){   //arm movement, nested to make it simpler
+
+    for(int i = 52; i <= 125; i++){
+
+      Arm.write(i);
+
+      delay(10);
+
+    }
+
+    for(int i = 125; i >= 52; i--){
+
+      Arm.write(i);
+
+      delay(10);
+
+    }
+
   }
 
-  digitalWrite(Rdrive1A, HIGH); 
+  analogWrite(Rena1, 0);
+
+  analogWrite(Lena1, 0);
+
+  delay(200);
+
+ 
+
+  for(int i= 0; i< 3; i++){ //claw dance
+
+    Claw.write(10); 
+
+    delay(100); 
+
+    Claw.write(55);
+
+    delay(100);
+
+  }
+
+ 
+
+  digitalWrite(Rdrive1A, HIGH);  //spin to the right and repeat
+
   digitalWrite(Rdrive2A, LOW);
 
-  digitalWrite(Ldrive1A, HIGH); 
+  digitalWrite(Ldrive1A, HIGH);
+
   digitalWrite(Ldrive2A, LOW);
 
-  for(int i = 50; i <= 125;i++){
-    Arm.write(i);
-  }
-  for(int i = 125; i >= 50;i--){
-    Arm.write(i);
+ 
+
+  analogWrite(Rena1, 200);
+
+  analogWrite(Lena1, 200);
+
+ 
+
+  for(int j = 0; j < 2; j++){ //repeats arm dance
+
+    for(int i = 52; i <= 125; i++){
+
+      Arm.write(i);
+
+      delay(10);  
+
+    for(int i = 125; i >= 52; i--){
+
+      Arm.write(i);
+
+      delay(10);
+
+    }
+
   }
 
-  digitalWrite(Ldrive1A, LOW); 
-  digitalWrite(Ldrive2A, HIGH);
+ 
+
+  digitalWrite(Rdrive1A, LOW); //backwaards
+
+  digitalWrite(Rdrive2A, HIGH);
+
+  digitalWrite(Ldrive1A, HIGH);
+
+  digitalWrite(Ldrive2A, LOW);
+
+ 
+
+  analogWrite(Rena1, 200);
+
+  analogWrite(Lena1, 200);
 
   delay(500);
 
-  analogWrite(Rena1, 255);
-  analogWrite(Lena1, 255);
+ 
+
+  analogWrite(Rena1, 0); //stop here at final movement
+
+  analogWrite(Lena1, 0);
+
+  Arm.write(90);
+
+  Claw.write(40); //putting YEAH by usher to w dance
+
 }
